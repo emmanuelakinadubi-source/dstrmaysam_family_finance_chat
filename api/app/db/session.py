e5903b8275c38d835877b9bc35464f301924ff13
Emmanuel Adubi <emmanuel.akin.adubi@gmail.com>
2026-06-17 14:28:15 +0100

    added requirements files

--- api/app/db/session.py
+++ api/app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/family_finance"
+DATABASE_URL = "postgresql://postgres:adubi1214@db:5432/family_expense"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
